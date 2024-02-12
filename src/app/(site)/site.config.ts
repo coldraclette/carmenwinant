ceef51d83c79f86464f30656176abc327767fdc1
Timo Tännler <timo.taennler@protonmail.com>
2024-02-12 17:13:19 +0200

    Add site configuration and generate metadata

---
+++ src/app/(site)/site.config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  title: 'Carmen Winant',
+  description:
+    'Carmen Winant is an artist and writer based in Columbus, OH.',
+  siteUrl: 'https://carmenwinant.com',
+  siteUrlShort: 'carmenwinant.com',
+};
